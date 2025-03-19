05ccf10261aa52a7de7a6a008c72d7d7a62af9ef
Aidana <aidannaa2007@gmail.com>
2025-03-19 21:07:59 +0500

    Add files via upload

---
+++ lab7b.py
@@ -0,0 +1,70 @@
+import pygame 
+
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((800, 480))
+
+COLOR_WHITE = (255, 255, 255)
+
+
+_songs = ['I_wanna_be_yours.mp3', 'Out_of_my_league.mp3', 'Who_says.mp3', 'Lover.mp3']
+
+
+def next_song(): # function for next_song
+    global _songs
+    _songs = _songs[1:] + [_songs[0]] # putting current song to the back of the list
+    pygame.mixer.music.load(_songs[0]) # loading the next song 
+    pygame.mixer.music.play() # playing the loaded song
+
+def previous_song():
+    global _songs
+    _songs = [_songs[-1]] + _songs[:-1] # putting the last-played song to the front of the list
+    pygame.mixer.music.load(_songs[0]) # loading the last-played song
+    pygame.mixer.music.play() # playing the loaded song
+
+paused = False
+
+def stopping(): # function for stopping the song
+    global paused
+    paused = not paused
+    if paused:
+        pygame.mixer.music.pause() # if the paused it True, call the pause() function 
+    else:
+        pygame.mixer.music.unpause() # if the paused is False, call unpause() function 
+
+clock = pygame.time.Clock()
+FPS = 60
+
+
+# pygame has only 24([0-23]) events and to add a custom event we add 1(and more to have more) to USEREVENT
+# USEREVENT has the value of 23
+# so, our new event would have an index of 24, and if we wanted to add more custom events, we would do it 
+# like this: NEW_EVENT = pygame.USEREVENT + 2
+# whenever the song ends 'set_endevent' pushes new event 'SONG_END' to the event queue
+# our game loop would detect the 'SONG_END' event and starts the 'next_song' function 
+SONG_END = pygame.USEREVENT + 1 
+pygame.mixer.music.set_endevent(SONG_END)
+
+pygame.mixer.music.load(_songs[0]) # loading the current song 
+pygame.mixer.music.play() # playing the loaded 'current' song right after the window opens
+
+running = True
+while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT: # next song
+                next_song() # calling next_song function
+            if event.key == pygame.K_LEFT: # previous song
+                previous_song() # calling the previous_song function 
+            if event.key == pygame.K_SPACE:
+                stopping() # calling the stopping() function 
+        if event.type == SONG_END:
+            next_song()
+
+    screen.fill(COLOR_WHITE) # filling the screen with white color
+
+    pygame.display.flip()
+    clock.tick(FPS)
---
+++ lab7c.py
@@ -0,0 +1,46 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 480
+
+screen = pygame.display.set_mode((800, 480))
+
+COLOR_WHITE = (255, 255, 255)
+COLOR_RED = (255, 0, 0)
+
+rect_x = (WIDTH - 50) // 2 # if we just used WIDTH // 2, the left edge of the circle would be at the center of the screen.
+                           # otherwise, it would we would've centered top-left corner of the circle
+rect_y = (HEIGHT - 50) // 2 # same with height
+
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[pygame.K_UP] and rect_y > 0: # if the UP arrow key is pressed and the coords are greater than 0 
+            rect_y -= 20                         # move the rectangle by 20 pixels to the top
+    if pressed_keys[pygame.K_DOWN] and rect_y < (HEIGHT - 50): # if the DOWN arrow key is pressed and coords are less than (HEIGHT - 50)
+            rect_y += 20                                       # move the rectangle by 20 px to the bottom(HEIGHT - 50, because, otherwise, we would go out of boundaries)
+    if pressed_keys[pygame.K_RIGHT] and rect_x < (WIDTH - 50): # if the RIGHT arrow key is pressed and coords are less than (WIDTH - 50)
+            rect_x += 20                                       # move the rectangle by 20 px to the right
+    if pressed_keys[pygame.K_LEFT] and rect_x > 0: # if the LEFT arrow key is pressed and coords are less than (WIDTH - 50)
+            rect_x -= 20                           # move the rectangle by 20 px to the right   
+
+
+    screen.fill(COLOR_WHITE)
+
+    pygame.draw.circle(screen, COLOR_RED, (rect_x + 25, rect_y + 25), 25) # drawing the moved circle on the screen at the given positions
+                                                                          # we write +25 because, we have to show
+                                                                          # the x, y coordinates of the center of the ball
+                                                                          # otherwise, if we write (rect_x, rect_y), we would've 
+                                                                          # shown the coordinates of top-left corner of the circle
+
+    pygame.display.flip()
+    clock.tick(60)
